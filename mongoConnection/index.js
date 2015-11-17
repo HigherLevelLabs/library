3c2467af4f2678175207ac9dfb7cce1aa8f0f2f9
Austin Johnson <austin@efowl.com>
2015-11-17 09:32:20 -0700

    code ready to turn in for hackathon

--- mongoConnection/index.js
+++ mongoConnection/index.js
@@ -2,16 +2,17 @@
 
 
     var ObjectId = mongodb.ObjectID;
-
+//CREATE A USER/CUSTOMER FUNCTION
     var CreateCustomer = function (collectionName, customer, callback) {
         connector.ConnectToDB(function (db, closeDB) {
-            db.collection(collectionName).insert(customer, function (err) {
+            db.collection(collectionName).insert(customer, function (err,doc) {
                 if (err) {
                     closeDB();
                     return console.log(err);
                 }
-
-                FindAllInCollectionAsArray("adduser", function (data) {
+                    
+                
+               FindSingleCollectionByID("users", doc.ops[0]._id, function (data) {
 
                     callback(data);
                 });
@@ -20,8 +21,8 @@
     };
 
 
-    
-    //CREATE THE BOOK
+
+    //CREATE A BOOK FUNCTION
     var CreateBook = function (collectionName, model, callback) {
         connector.ConnectToDB(function (db, closeDB) {
             db.collection(collectionName).insert(model, function (err) {
@@ -36,23 +37,37 @@
         });
     };
 
-
+    
+//LOCATE A CUSTOMER BY ID
     var FindCustomerByID = function (collectionName, customer, callback) {
+
         connector.ConnectToDB(function (db, closeDB) {
-            db.collection(collectionName).find({
-                customer: new ObjectID(customer)
-            }).toArray(
-                function (err, data) {
-                    if (err) {
-                        closeDB();
-                        return console.log(err);
-                    }
-                    callback(data);
+
+            db.collection(collectionName).find(
+
+                {
+                    email: customer.email,
+                    password: customer.password
+
+                }
+
+            ).toArray(function (err, data) {
+                if (err) {
                     closeDB();
-                });
+                    console.log(err);
+                }
+
+
+                callback(data);
+
+                closeDB();
+
+            });
+
         });
     };
 
+//LOAD ALL BOOKS FOR BROWSING THE LIBRARY
     var FindAllInCollectionAsArray = function (collectionName, callback) {
 
         connector.ConnectToDB(function (db, closeDB) {
@@ -82,13 +97,16 @@
 
     };
 
+    
+//FUNCTION TO PULL SINGLE USER DATA FOR LOGIN - NEED TO REPLACE WITH PASSPORT    
     var FindSingleCollectionByID = function (collectionName, id, callback) {
 
 
         connector.ConnectToDB(function (db, closeDB) {
 
             db.collection(collectionName).find({
-
+                    
+                _id: id
 
             }).toArray(function (err, data) {
 
@@ -99,7 +117,7 @@
 
                 console.log(" Connected correctly to Mongo Server ");
 
-                callback(data);
+                callback(data[0]);
 
                 console.log(" Custom passed-in callback fired ");
 
@@ -113,6 +131,7 @@
         });
     }
 
+//FUNCTION TO CHECKOUT A LIBRARY      
     var UpdateSingleCollectionByID = function (collectionName, model, callback) {
 
 
@@ -124,9 +143,9 @@
 
                 {
                     $set: {
-                         status: model.status,
-                         co_date: model.co_date,
-                         due_date: model.due_date,
+                        status: model.status,
+                        co_date: model.co_date,
+                        due_date: model.due_date,
                     }
                 }, {},
                 function (err) {
@@ -155,18 +174,13 @@
 
     }
 
-    dbRepo.CreateCustomer = CreateCustomer;
-
     
+//DB REPOS    
+    dbRepo.CreateCustomer = CreateCustomer;
     dbRepo.CreateBook = CreateBook;
-
-
     dbRepo.FindCustomerByID = FindCustomerByID;
-
     dbRepo.FindAllInCollectionAsArray = FindAllInCollectionAsArray;
-
     dbRepo.FindSingleCollectionByID = FindSingleCollectionByID;
-
     dbRepo.UpdateSingleCollectionByID = UpdateSingleCollectionByID;
 
 })(
--- server.js
+++ server.js
@@ -2,15 +2,16 @@
 
 
 
-    //get,put,post,delete
+    //Intantiate body-parser for form reading
     server.use(bodyParser.urlencoded({
         extended: true
     }));
 
-
+    //call our assets folder including CSS and IMG for use accross all HTML documents
     server.use(express.static('public'));
 
 
+    //homepage route to load home.html    
     server.get("/", function (req, res) {
         fs.readFile("templates/home.html", function (err, data) {
             if (err) {
@@ -22,6 +23,7 @@
     });
 
 
+    //route all library books onto the stage    
     server.get("/browse", function (req, res) {
         //do checks
         var content = "";
@@ -42,11 +44,43 @@
 
     });
 
+    //INCOMPLETE ROUTE - Put Function    
+    server.put("/browse", function (req, res) {
+        var model = myData.filter(function (value) {
+            return value.id == req.body.id;
+        })[0];
+
+        model.name = req.body.name;
+        model.bio = req.body.bio;
+
+        //db update
+        res.json({
+            success: myData,
+            failure: false
+        });
+    });
+
+    //INCOMPLETE ROUTE - Delete Function
+    server.delete("/browse", function (req, res) {
+
+        var model = myData.filter(function (value) {
+            return value.id == req.body.id;
+        })[0];
+
+        model.name = "DELETED";
+        model.bio = "DELETED";
+
+        res.json({
+            success: myData,
+            failure: false
+        });
+    });
 
 
+    //route performs a checkout on a library book
     server.post("/checkout", function (req, res) {
         //update in the DB
-        
+
         var model = {
             id: req.body.id,
             status: "out",
@@ -64,9 +98,65 @@
 
     });
 
-    
-    
-    
+    //LOGIN ROUTES - 75% functioning, need to deploy Passport
+    server.get("/login", function (req, res) {
+        fs.readFile("templates/login.html", function (err, data) {
+            if (err) {
+                console.log(err);
+                return res.send("ICEBERG");
+            }
+            res.send(data.toString());
+        });
+    });
+
+    //still needs to be built out - incomplete
+    server.post("/login", function (req, res) {
+        var model = {
+            email: req.body.email,
+            password: req.body.password
+        }; //update in the DB
+
+        console.log("The model is : " + JSON.stringify(model));
+
+        mongoRepo.FindCustomerByID("users", model, function (data) {
+            var response = "";
+
+            if (data == 'undefined') {
+
+                response = "Bad username or PW";
+
+            } else {
+                response = "User has logged in";
+                response.success = data;
+            }
+            /*
+                        var obj = {
+                            success: data,
+                            failure: false
+                        };
+                        
+                        console.log(data);
+                        if (data[0] == 'undefined') {
+
+                            console.log("Bad username or PW");
+                            obj.errormsg = "Bad username or PW"
+                            obj.failure = true;
+                        } 
+
+
+
+                       
+                        
+                        */
+
+            res.json(response);
+        });
+
+    });
+    //END LOGIN FUNCTIONS  
+
+
+    //ADD BOOK ROUTES - creates a new book   
     server.get("/addbook", function (req, res) {
         fs.readFile("templates/addbook.html", function (err, data) {
             if (err) {
@@ -76,42 +166,6 @@
             res.send(data.toString());
         });
     });
-	
-	server.get("/adduser",function(req,res){
-		fs.readFile("templates/adduser.html", function(err,data){
-			if(err){
-				console.log(err);
-				return res.send("DIDN'T WORK!");
-			}
-			res.send(data.toString());
-		});
-	});
-
-    server.post("/adduser", function (req, res) {
-        var model = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            phone: req.body.phone,
-            password: req.body.pwd
-        };//update in the DB
-
-        console.log("Add user Called");
-
-
-        mongoRepo.CreateCustomer("users", model, function (data) {
-
-
-
-
-            res.json({
-                success: data,
-                failure: false
-            });
-        });
-
-    });
-
 
     server.post("/addbook", function (req, res) {
         var model = {
@@ -120,15 +174,15 @@
             author: req.body.author,
             synopsis: req.body.synopsis,
             status: req.body.status,
-			rating: req.body.rating,
-			checkOutDate: req.body.checkOutDate,
-			dueDate: req.body.dueDate,
-			customer: req.body.customer
+            rating: req.body.rating,
+            checkOutDate: req.body.checkOutDate,
+            dueDate: req.body.dueDate,
+            customer: req.body.customer
         };
-        
-        
+
+
         console.log("the req.body.name : " + req.body.title);
-        
+
         console.log("the model is: " + JSON.stringify(model));
 
         mongoRepo.CreateBook("library", model, function (data) {
@@ -138,41 +192,52 @@
             });
         });
     });
+    //END ADD BOOK FUNCTIONS
 
-
-
-    server.put("/browse", function (req, res) {
-        var model = myData.filter(function (value) {
-            return value.id == req.body.id;
-        })[0];
-
-        model.name = req.body.name;
-        model.bio = req.body.bio;
-
-        //db update
-        res.json({
-            success: myData,
-            failure: false
-        });
-    });
-
-    server.delete("/browse", function (req, res) {
-
-        var model = myData.filter(function (value) {
-            return value.id == req.body.id;
-        })[0];
-
-        model.name = "DELETED";
-        model.bio = "DELETED";
-
-        res.json({
-            success: myData,
-            failure: false
+    //ADD A USER ROUTES - creates a new user     
+    server.get("/adduser", function (req, res) {
+        fs.readFile("templates/adduser.html", function (err, data) {
+            if (err) {
+                console.log(err);
+                return res.send("DIDN'T WORK!");
+            }
+            res.send(data.toString());
         });
     });
 
 
+    server.post("/adduser", function (req, res) {
+        var model = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.pwd
+        }; //update in the DB
 
+        console.log("Add user Called");
+
+
+        mongoRepo.CreateCustomer("users", model, function (data) {
+
+
+            res.json({
+                success: data,
+                failure: false
+            });
+        });
+
+    });
+    //END ADD USER FUNCTIONS
+
+
+
+
+
+
+
+
+    //Listen function - keep at that bottom of all routes
     server.listen(PORT, IP, function () {
         console.log(" server online ");
     });
